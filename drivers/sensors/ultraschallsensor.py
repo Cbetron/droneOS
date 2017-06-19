774d85382434eb68a837606197fadd908de62bb8
D3D53C <marc.steinebrunner@gmail.com>
2017-06-19 18:28:03 +0200

    Update ultraschallsensor.py

--- drivers/sensors/ultraschallsensor.py
+++ drivers/sensors/ultraschallsensor.py
@@ -1,4 +1,14 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+"""ultraschallsensor.py:	Ultrashallsensor class"""
+
+__author__ = "Raphael Kreft"
+__copyright__ = ""
+__version__ = "Development v0.01"
+__email__ = "raphaelkreft@gmx.de"
+__status__ = "Dev"
+
 
 import time
 import RPi.GPIO as gpio
@@ -7,52 +17,20 @@ from ...config import config
 
 
 class Ultraschallsensor(object):
-    """
-    Author: Raphael Kreft
 
-    Diese Klasse abstrahiert einen Ultraschallsensor vom Typ: HC-SR04
-    """
+    #Diese Klasse abstrahiert einen Ultraschallsensor vom Typ: HC-SR04
 
     def __init__(self, identifier=__name__):
-        """
-        Diese Funktion wird direkt nach der Erzeugung eines Objekts dieeser Klasse aufgerufen.
-        Hierbei werden die Anschlusspins des Ultraschallsensors mit Hilfe der Funktion "setPins"
-        gesetzt.
-
-        Args:       -
-
-        Returns:    -
-        """
         self.ID = identifier
         pins = Config.getPins(self.ID)
+
         self.GPIO_Trigger = pins[0]
         self.GPIO_Echo = pins[1]
-        self.set_pins(self.GPIO_Trigger, self.GPIO_Echo)
 
-    def set_pins(self, gpio_trigger, gpio_echo):
-        """
-        Mit Hilfe dieser Funktion können die Anschlusspins des Ultraschallsensors
-        gesetzt werden.
-
-        Args:       GPIO_Trigger    Der Anschlusspin des Sensors mit dem gesendet wird
-                    GPIO_Echo       Der Pin an dem später die Eintreffenden Daten ausgelesen werden
-
-        Returns:    -
-        """
-        self.GPIO_Trigger = gpio_trigger
-        gpio.setup(gpio_trigger, gpio.OUT)
-        self.GPIO_Echo = gpio_echo
-        gpio.setup(gpio_echo, gpio.IN)
+        gpio.setup(self.GPIO_Trigger,gpio.OUT)
+        gpio.setup(self.GPIO_Echo,gpio.IN)
 
     def distanz(self):
-        """
-        Diese Funktion nutzt den Ultraschallsensor zur ermittlung der aktuellen
-        Distanz von etwas.
-
-        Args:       -
-
-        Returns:    distanz     Die errechnete Distanz, die nun Zurrückgegeben wird
-        """
         gpio.output(GPIO_Trigger, True)  # Der Trigger versendet ein Signal
         time.sleep(0.0001)
         gpio.output(GPIO_Trigger, False)
@@ -66,7 +44,4 @@ class Ultraschallsensor(object):
         while gpio.input(GPIO_Echo) == 1:  # Stopzeit speichern
             stopzeit = time.time()
 
-        zeitspanne = stopzeit - startzeit  # Distanz berechnen
-        distanz = (zeitspanne * 34300) / 2
-
-        return distanz
+        return ((stopzeit - startzeit) * 34300) / 2
